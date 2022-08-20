fbf806010943358d529364c2a130d1dc825c0326
jyoti <jyotidhage10@gmail.com>
2022-08-20 19:17:13 +0530

    [Aaradhya]Add: Complete Object Oriented Programs

---
+++ FetchJsonForRice.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInventoryManagement1
+{
+    public class FetchJsonForRice
+    {
+        public Rice Read (string Path)
+         {
+            using (StreamReader file = new StreamReader(Path)) {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+        }
+    }
+}
---
+++ Rice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInventoryManagement1
+{
+    public class Rice
+    {
+        public List<TypesOfRice> typesOfRice;
+        public string name;
+        public string weight;
+        public string price;
+
+
+
+        public List<TypesOfPulses> typesOfpulses;
+        public string name;
+        public string weight;
+        public string price;
+
+
+        public List<TypesOfWheat> typesOfWheat;
+        public string name;
+        public string weight;
+        public string price;
+
+
+
+
+    }
+}
