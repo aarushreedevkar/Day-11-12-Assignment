813139b1a85a0b1173d32d9716d6be56acbf798b
jyoti <jyotidhage10@gmail.com>
2022-08-20 19:21:13 +0530

    [Aardhya]Add:complete Object Oriented Programs

---
+++ StockAccountManagement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace DataInventoryManagement1
+//{
+//    public  class StockAccountManagement
+//    {
+//        private String companyName;
+//        private String stockRating;
+//        private int price;
+//        private int numberOfShares;
+
+//        public String getCompanyName()
+//        {
+//            return companyName;
+//        }
+
+//        public int getStockRating()
+//        {
+//            return stockRating;
+//        }
+
+//        public String getPrice()
+//        {
+//            return price;
+//        }
+
+//        public int getNumberOfShares()
+//        {
+//            return numberOfShares;
+//        }
+
+//        public Stock(String companyName, String stockRating, int price, int numberOfShares)
+//        {
+//            super();
+//            this.companyName = companyName;
+//            this.stockRating = stockRating;
+//            this.price = price;
+//            this.numberOfShares = numberOfShares;
+//        }
+//    }
+//}
