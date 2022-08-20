edd16d028513410c8fdd0f1c76e35499de8e64d7
jyoti <jyotidhage10@gmail.com>
2022-08-20 23:57:58 +0530

    [Aardhya]Add:complete Object Oriented Programs

---
+++ CompanyShares.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInventoryManagement1
+{
+    public class CompanyShares
+    {
+        public static void ListOfCompanyShares()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("ordinary shares");
+            list.AddLast("Nonvoting shares");
+            list.AddLast("Preferance shares");
+            list.AddLast("Redeemable Shares");
+
+            foreach (string shares in list)
+            {
+                Console.WriteLine(shares);
+            }
+        }
+    }
+}
