d5e551f94dca63eba34cfadf627cb46aae30698b
jyoti <jyotidhage10@gmail.com>
2022-08-20 19:23:24 +0530

    [Aardhya]Add:complete Object Oriented Programs

--- StockAccountManagement.cs
+++ StockAccountManagement.cs
@@ -4,42 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace DataInventoryManagement1
-//{
-//    public  class StockAccountManagement
-//    {
-//        private String companyName;
-//        private String stockRating;
-//        private int price;
-//        private int numberOfShares;
+namespace DataInventoryManagement1
+{
+    public class StockAccountManagement
+    {
+        private String companyName;
+        private String stockRating;
+        private int price;
+        private int numberOfShares;
 
-//        public String getCompanyName()
-//        {
-//            return companyName;
-//        }
+        public String getCompanyName()
+        {
+            return companyName;
+        }
 
-//        public int getStockRating()
-//        {
-//            return stockRating;
-//        }
+        public int getStockRating()
+        {
+            return stockRating;
+        }
 
-//        public String getPrice()
-//        {
-//            return price;
-//        }
+        public String getPrice()
+        {
+            return price;
+        }
 
-//        public int getNumberOfShares()
-//        {
-//            return numberOfShares;
-//        }
+        public int getNumberOfShares()
+        {
+            return numberOfShares;
+        }
 
-//        public Stock(String companyName, String stockRating, int price, int numberOfShares)
-//        {
-//            super();
-//            this.companyName = companyName;
-//            this.stockRating = stockRating;
-//            this.price = price;
-//            this.numberOfShares = numberOfShares;
-//        }
-//    }
-//}
+        public Stock(String companyName, String stockRating, int price, int numberOfShares)
+        {
+            super();
+            this.companyName = companyName;
+            this.stockRating = stockRating;
+            this.price = price;
+            this.numberOfShares = numberOfShares;
+        }
+    }
+}
